7d6fc7e200cefe45d2a3ba43d7b123b605123f9e
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2022-08-29 17:33:33 +0530

    Add files via upload

---
+++ PI.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#define PI 3.14
+
+void main()
+	{
+		int a=10;
+		int b=20;
+		clrscr();
+		printf("first program of c");
+		printf("\na=%d",a);
+		printf("\nb=%d",b);
+		printf("\nPI=%f",PI);
+		getch();
+	   }
+
+
