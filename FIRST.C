3f9b391dfb0fa4392fa975632d6e787716c7db4d
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2022-08-30 17:54:57 +0530

    Add files via upload

---
+++ FIRST.C
@@ -0,0 +1,19 @@
+#include<stdio.h>    //standerd input output
+#include<conio.h>   //console input output
+//definition section
+#define PI 3.14     //symfolic constant
+//global declaration sction
+int a=10;
+void main() //main faction sction
+{
+  //declaration part
+  int b=30;
+  //executable part
+  clrscr();
+  printf("first program of c");
+  printf("\n a=%d",a);
+  printf("\n b=%d",b);
+  printf("\n PI=%f",PI);
+  getch();
+ }
+
