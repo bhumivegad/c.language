780800e1f35c9bc08321de8ea8af7a869ec00505
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2022-08-31 17:43:39 +0530

    Add files via upload

---
+++ ADDITION.C
@@ -0,0 +1,14 @@
+// ADDITION
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c;
+	clrscr();
+	printf("enter value of two numbers :");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("\n sum of two num=%d",c);
+	getch();
+}
