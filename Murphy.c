9979f4930d752cdcb4492573909ccaa2d64f5c1b
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2022-09-17 11:57:53 +0530

    Create Murphy.c

---
+++ Murphy.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+void main ()
+{
+    int n,a,b=0,x;
+
+    printf("enter n");
+    scanf("%d",&n);
+    x=n;
+    while(n>0)
+      {
+        a=n%10;
+        b=b+a*a*a;
+        n=n/10;
+      }
+        if(b==x)
+        {
+          printf("Armstrong no");
+        }
+        else
+        {
+           printf("not no.");
+        }
+        
+     getchar();
+    
+    
+    
+ }
+
+    
