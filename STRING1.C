2ce215a34e30b5bbf62dbfa023d48ba901013acb
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2022-09-26 17:45:29 +0530

    Add files via upload

---
+++ STRING1.C
@@ -0,0 +1,15 @@
+//string ex.1
+
+void main()
+{
+	char s1[20],s2[20],s3[20];
+	clrscr();
+	puts("enter a string:");
+	gets(s1);
+	puts("enter a string:");
+	gets(s2);
+	puts("enter a string:");
+	gets(s3);
+	printf("\n s1=%s \n s2=%s \n s3=%s",s1,s2,s3);
+	getch();
+}
