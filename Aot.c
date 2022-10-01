4021c1ec5193226f0b204bea99f07c245ea3e48b
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2022-10-01 12:18:12 +0530

    Create Aot.c

---
+++ Aot.c
@@ -0,0 +1,15 @@
+// area of triangle 
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	double b,h,aot;
+	clrscr();
+	printf("Enter value b,h");
+	scanf("%lf %lf",&b,&h);
+	aot=b*h/2;
+	printf("aot is %lf",aot);
+	getchar();
+}
+    
