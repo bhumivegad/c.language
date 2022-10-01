b1ae66bcb5adf5787f48370e0f5ff466b28d6bc3
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2022-10-01 11:57:52 +0530

    Create Aoc.c

---
+++ Aoc.c
@@ -0,0 +1,15 @@
+//
+
+#include<stdio.h>
+#include<conio.h>
+#define PI 3.14
+void main()
+{
+	double r,aoc;
+	clrscr();
+	printf("Enter value of r");
+	scanf("%lf",&r);
+	aoc=PI*r*r;
+	printf("aoc is %lf",aoc);
+	getch();
+}
