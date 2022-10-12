2f38abffbbf29e1ee652fb1ad1487d5cdc9869d3
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2022-10-12 21:49:30 +0530

    Update and rename REFERNCE.C to REFERENCE.C

--- REFERENCE.C
+++ REFERENCE.C
@@ -6,9 +6,9 @@
 	int b;
 	clrscr();
 	printf("enter two no:");
-	scanf("%d %ld",&a,&b);
-	printf("\n before call by refernce:");
-	printf("\n a=%d \t b=%ld");
+	scanf("%d %d",&a,&b);
+	printf("\n before call by reference:");
+	printf("\n a=%d \t b=%d");
 	swap(a,b);
 	printf("\n after calling:");
 
@@ -19,5 +19,5 @@
    c=a;
    a=b;
    b=c;
-   printf("\n inside swap funtion a=%d \t b=%ld",a,b,c);
+   printf("\n inside swap funtion a=%d \t b=%d",a,b,c);
 }
