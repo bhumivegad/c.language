234cebae0b569d8668027067e88833c18fddb31d
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2022-10-14 17:06:19 +0530

    Add files via upload

---
+++ 57.C
@@ -0,0 +1,20 @@
+ // 57
+ #include<stdio.h>
+ void main()
+ {
+	int b,max,min;
+	clrcsr();
+	printf("positive no");
+	scanf("%d",&b);
+	if(b<=0)
+	{
+		printf("negative");
+	}
+	max=0;
+	main=0;
+	for(i=1;i<n;i++)
+	{
+		max=
+	}
+
+ }
