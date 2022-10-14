8f2553471ccb70efc68cb1b638f07f2baa6e18d8
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2022-10-14 15:43:42 +0530

    Add files via upload

---
+++ 94.C
@@ -0,0 +1,22 @@
+
+//94
+void main()
+{
+	int a[5],i,*p;
+	clrscr();
+	p=a;
+	for(i=0;i<5;i++)
+	{
+		printf("entre a[%d]:",i);
+		scanf("%d",p);
+		p++;
+	}
+	p=a;
+	for(i=0;i<5;i++)
+	{
+		printf("\n a[%d]=%d",i,*p);
+		p++;
+	}
+	getch();
+}
+
